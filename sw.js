b9b46cca515f7ded578adc95b71b599495deea4b
HamzaAhmedSheikh <hamzaahmedsheikh313@gmail.com>
2019-07-29 02:30:01 +0500

    Progressive Web App

---
+++ sw.js
@@ -0,0 +1,43 @@
+var cacheName = 'my-demo-app_02';
+
+var filesToCache = [
+  '/style.css',
+  '/index.js',
+  '/index.html',
+  '/',
+];
+
+
+self.addEventListener('install', function(e) {
+     console.log('[ServiceWorker] Install')
+    e.waitUntil(
+        caches.open(cacheName).then(function(cache) {
+          console.log('[ServiceWorker] Caching app shell');
+          return cache.addAll(filesToCache);
+        })
+      );
+  });
+
+  self.addEventListener('activate', function(e) {
+    console.log('[ServiceWorker] Activate');
+e.waitUntil(
+caches.keys().then(function(keyList){
+  return new Promise.all(keyList.map(function(key){
+      if(key !== cacheName){
+       console.log('[ServiceWorker] Removing old cache', key);
+         return caches.delete(key);
+      }
+  }));
+ })
+);
+        return self.clients.claim();      
+});
+
+self.addEventListener('fetch', function(e) {
+  console.log('[ServiceWorker] Fetch', e.request.url);
+e.respondWith(
+ caches.match(e.request).then(function(response){
+     return response || fetch(e.request);
+ })
+);
+});
